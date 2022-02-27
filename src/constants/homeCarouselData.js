b58bfe6ac51e204ab04b0186089957d6f79b0e08
do <do@mission.plus>
2022-02-27 20:01:06 +0700

    add project

---
+++ src/constants/homeCarouselData.js
@@ -0,0 +1,29 @@
+/**
+ * Fake Landscape hinhAnh data for home carousel (API doesnt return landscape data)
+ */
+
+const homeCarouselData = [
+  {
+    hinhAnh: "img/carousel/gai-gia-lam-chieu.jpg",
+    trailer: "https://www.youtube.com/embed/krgcyk2rjFc",
+    tenPhim: "Gái Già Lắm Chiêu",
+    biDanh: "gai-gia-lam-chieu",
+    maPhim: 9913,
+  },
+  {
+    hinhAnh: "img/carousel/lua-deu-gap-lua-dao.jpg",
+    trailer: "https://www.youtube.com/embed/74vbhMvB9Ak",
+    tenPhim: "Lừa Đểu Gặp Lừa Đảo",
+    biDanh: "lua-deu-gap-lua-dao",
+    maPhim: 9914,
+  },
+  {
+    hinhAnh: "img/carousel/than-bip-jack-chot.jpg",
+    trailer: "https://www.youtube.com/embed/gqcpChNYH10",
+    tenPhim: "Thần Bịp Jack Chột",
+    biDanh: "than-bip-jack-chot",
+    maPhim: 9915,
+  },
+];
+
+export default homeCarouselData;
